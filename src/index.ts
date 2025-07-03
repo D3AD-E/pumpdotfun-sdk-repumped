ff0f8d174a79560b6af31f00de70cd0234352e20
Yevhenii <evg.gordi@gmail.com>
2025-07-03 20:23:40 +0200

    Export jito

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from "./PumpFunSDK.js";
 export * from "./modules/TradeModule.js";
 export * from "./modules/TokenModule.js";
 export * from "./modules/EventModule.js";
+export * from "./modules/JitoModule.js";
 
 export * from "./BondingCurveAccount.js";
 export * from "./globalAccount.js";
